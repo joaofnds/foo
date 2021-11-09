02d17db947c16f80ca1585c4cd9bf9ba8e7d964f
João Fernandes <joaofnds@joaofnds.com>
2021-11-09 20:01:43 -0300

    fix: remove db conn print

--- db.go
+++ db.go
@@ -9,7 +9,6 @@ import (
 
 func GetConn(host, port, user, password, database string) (*sql.DB, error) {
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
-	fmt.Println("conn: " + conn)
 
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
